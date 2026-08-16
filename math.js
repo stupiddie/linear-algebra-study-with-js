ae0906aed63d9f45db522bf6e1baab6108b61990
stupiddie <chenshengyu10@gmail.com>
2026-08-16 17:21:15 +0800

    Update math.js
    function (...) {
         return ...
    }

--- math.js
+++ math.js
@@ -6,14 +6,14 @@ function add(...v) {
        sum[1] += v[i][1];
     }
 
-    console.log(sum);
+    return sum;
 }
 
 function multi(x, n) {
     var result = [x[0] * n, x[1] * n];
 
-    console.log(result);
+    return result;
 }
 
-add([9, 9], [8, 8], [1, 1]);
-multi([3, 3], 3);
+console.log(add([9, 9], [8, 8], [1, 1]));
+console.log(multi([3, 3], 3));
