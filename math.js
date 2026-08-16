db4b155b9544dcafac9b214aa037adc2570d911f
陈胜宇 <chenshengyu10@gmail.com>
2026-08-16 17:07:34 +0800

    day01

---
+++ math.js
@@ -0,0 +1,19 @@
+function add(...v) { 
+    var sum = [0, 0];
+
+    for (var i = 0; i < v.length; i++) {
+       sum[0] += v[i][0];
+       sum[1] += v[i][1];
+    }
+
+    console.log(sum);
+}
+
+function multi(x, n) {
+    var result = [x[0] * n, x[1] * n];
+
+    console.log(result);
+}
+
+add([9, 9], [8, 8], [1, 1]);
+multi([3, 3], 3);
